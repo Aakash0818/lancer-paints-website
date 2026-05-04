8090990edadfe771432554db9abc4e2d30a8321d
Aakash0818 <aakashraj2003@gmail.com>
2026-05-05 00:14:15 +0530

    added social preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,20 @@ export const metadata = {
   title: "Lancer Paints Pvt Ltd - Premium Paints in Hyderabad",
   description:
     "Interior, exterior primers, wall putty, cement paints, and pigments for durable and premium finishes.",
+  openGraph: {
+    title: "Lancer Paints Pvt Ltd",
+    description: "Premium paints for interior & exterior use",
+    url: "https://your-vercel-link.vercel.app",
+    siteName: "Lancer Paints",
+    images: [
+      {
+        url: "/logo.jpg",
+        width: 800,
+        height: 600,
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({
